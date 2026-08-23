0d7a5b7d91367f1954361ceb57a005e822079a51
huixiaw012 <126255306+huixiaw012@users.noreply.github.com>
2026-08-23 16:46:53 +0200

    Refactor simulation for MCAR dataset handling
    Updated simulation description and modified library loading. Removed unused library paths and sources, and adjusted missing data handling to reflect MCAR.

--- MVN_MVN_MCAR1.R
+++ MVN_MVN_MCAR1.R
@@ -1,16 +1,8 @@
-###### This simulation is applied to evaluate of our method when the dataset is under country-level attrition setting
+###### This simulation is applied to evaluate of our method when the dataset is under missing complete at random setting (MCAR)
 #### Specifically, set X13-X15 and hY3 also have 5 clusters attrition randomly.
 #### Here set beta_j and beta_j+1 are non-linear relationship
 
 num_chains <- c(1:1000)  + 8e6
-
-.libPaths(c(
-  "/pfs/stor10/users/home/h/huixiaw/huixiaw/GcomGSBART",  # SBart2 + some deps
-  "/home/h/huixiaw/huixiaw/install_package",              # rBeta2009
-  Sys.getenv("R_LIBS_USER"),                              # your main user library
-  .libPaths()
-))
-
 
 k <- 10
 n_attr <- 5
@@ -42,17 +34,7 @@ Sig<-matrix(c(c(1,0,0,0,0,a1,0,0,0,0,a2,0,0,0,0),
               c(0,0,0,0,a2,0,0,0,0,a1,0,0,0,0,1)), 15, 15, byrow = TRUE)
 
 
-
-
-#suppressMessages(library(SBart2, lib.loc="/pfs/stor10/users/home/h/huixiaw/huixiaw/GcomGSBART/"))
-#suppressMessages(library(foreach))
-#suppressMessages(library(doParallel))
-#suppressMessages(library(dplyr))
-#suppressMessages(library(MASS))
-#suppressMessages(library(locfit))
-#suppressMessages(library(rBeta2009, lib.loc = "/home/h/huixiaw/huixiaw/install_package/"))
-
-library(SBart2)
+library(glBART)
 library(foreach)
 library(doParallel)
 library(dplyr)
@@ -74,48 +56,20 @@ n_tree <- 20 # may need to increase
 opts <- Opts(num_burn = n_burn, num_save = n_save)
 
 
-# Register a parallel backend
-#registerDoParallel(cores = 16)
-
 cl <- parallel::makeCluster(16)
 doParallel::registerDoParallel(cl)
 
 # 1) load package + source once per worker
 invisible(parallel::clusterEvalQ(cl, {
-  #library(SBart2)
-  #library(rBeta2009)
-  # source("/Users/huwa0014/Documents/Umu/Research/Positivity_violation/Simulation/ATE_bias_simulation/with_clusters/data_gen_good.R")
-  #source("~/Documents/Umu/Research/Positivity_violation/Simulation/ATE_bias_simulation/with_clusters/Y_spec_regime.R")
-  # source("/Users/huwa0014/Documents/Umu/Research/Positivity_violation/Simulation/ATE_bias_simulation/with_clusters/pgs_trim_by_group.R")
-  #  source("/Users/huwa0014/Documents/Umu/Research/G-computation/GcomGSBART/Rfunctions_pgs_riBART.R")
-  #  source("/Users/huwa0014/Documents/Umu/Research/G-computation/GcomGSBART/gc_pgs_riSBART_functions.R")
-  #  source("/Users/huwa0014/Documents/Umu/Research/G-computation/GcomGSBART/gc_gsoftbart_regression.R")
-  #  source("/Users/huwa0014/Documents/Umu/Research/G-computation/GcomGSBART/gc_gsoftbart_probit.R")
-  #  source("/Users/huwa0014/Documents/Umu/Research/G-computation/GcomGSBART/gc_softbart_regression.R")
-  #  source("/Users/huwa0014/Documents/Umu/Research/G-computation/GcomGSBART/gc_softbart_probit.R")
-  #  source("/Users/huwa0014/Documents/Umu/Research/G-computation/GcomGSBART/predict_function.R")
-  .libPaths(c(
-    "/pfs/stor10/users/home/h/huixiaw/huixiaw/GcomGSBART",  # SBart2 + deps
-    "/home/h/huixiaw/huixiaw/install_package",              # rBeta2009
-    Sys.getenv("R_LIBS_USER"),                              # main user lib (dplyr, locfit, etc.)
-    .libPaths()
-  ))
-  
-  library(SBart2)
+ 
+  library(glBART)
   library(rBeta2009)
   library(dplyr)
   library(MASS)
   library(locfit)
   library(rBeta2009)
-  source("/home/h/huixiaw/huixiaw/GcomGSBART_simulation/MAR_countries/nonlinear/gamma309016_MVNBeta/MVN_MVN_MCAR/sim_data_nonlin.R")
-  source("/home/h/huixiaw/huixiaw/GcomGSBART/Rfunctions_riBART.R")
-  source("/home/h/huixiaw/huixiaw/GcomGSBART/gc_riSBART_functions.R")
-  source("/home/h/huixiaw/huixiaw/GcomGSBART/gc_gsoftbart_regression.R")
-  source("/home/h/huixiaw/huixiaw/GcomGSBART/gc_gsoftbart_probit.R")
-  source("/home/h/huixiaw/huixiaw/GcomGSBART/gc_softbart_regression.R")
-  source("/home/h/huixiaw/huixiaw/GcomGSBART/gc_softbart_probit.R")
-  source("/home/h/huixiaw/huixiaw/GcomGSBART/predict_function.R")
-}) )
+  source("sim_data_nonlin.R")
+  }) )
 
 
 results <- foreach(i = num_chains, .combine = rbind) %dopar% {
@@ -136,11 +90,6 @@ results <- foreach(i = num_chains, .combine = rbind) %dopar% {
   dat_with_NA <- dat$df ### original data
 
   # Compute missing probability for each group
-  #missing_prob <- expit(-1 + 0.2*(-dat$Beta[,2]  ) ) # Moderate
- 
-  #missing_prob <- expit(-1.0 + 3.5 * (-dat$Beta[,2]) ) # severe
-
-  #miss_countries <- sample(1:k, size = n_attr, replace = FALSE, prob = missing_prob) ### Randomly MAR
   miss_countries <- sample(1:k, size = n_attr, replace = FALSE) ### Randomly MCAR
   
 
@@ -195,11 +144,7 @@ results <- foreach(i = num_chains, .combine = rbind) %dopar% {
 
 names(results) <- c(paste0("true_Y", 1:3), paste0("est_Y", 1:3))
 
-write.table(results,
-            file =  paste0("/home/h/huixiaw/huixiaw/GcomGSBART_simulation/MAR_countries/nonlinear/gamma309016_MVNBeta/MVN_MVN_MCAR/outputs/MVN_MVN_", n_attr ,"Miss","_Chain",
-                           min(num_chains), "_", max(num_chains)),
-            append=FALSE, col.names = TRUE, row.names = FALSE )
-
+print(results)
 
 # Stop the parallel backend
 stopImplicitCluster()
