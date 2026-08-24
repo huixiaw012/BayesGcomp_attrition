36447540cf7a41a37b2fd9da575c0a9163ef9855
huixiaw012 <126255306+huixiaw012@users.noreply.github.com>
2026-08-24 17:25:01 +0200

    Replace glBART with GcompGlBART library

--- MCAR_analysis.R
+++ MCAR_analysis.R
@@ -34,7 +34,7 @@ Sig<-matrix(c(c(1,0,0,0,0,a1,0,0,0,0,a2,0,0,0,0),
               c(0,0,0,0,a2,0,0,0,0,a1,0,0,0,0,1)), 15, 15, byrow = TRUE)
 
 
-library(glBART)
+library(GcompGlBART)
 library(foreach)
 library(doParallel)
 library(dplyr)
@@ -62,7 +62,7 @@ doParallel::registerDoParallel(cl)
 # 1) load package + source once per worker
 invisible(parallel::clusterEvalQ(cl, {
  
-  library(glBART)
+  library(GcompGlBART)
   library(rBeta2009)
   library(dplyr)
   library(MASS)
